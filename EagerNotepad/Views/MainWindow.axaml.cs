8afec5b4f7dcfdb24fe2f883c5c29772a8a5079f
Ed Kolis <edkolis@gmail.com>
2021-02-22 21:47:14 -0500

    Well I sort of have it working... also need more words...

--- EagerNotepad/Views/MainWindow.axaml.cs
+++ EagerNotepad/Views/MainWindow.axaml.cs
@@ -1,3 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
@@ -21,10 +25,42 @@ namespace EagerNotepad.Views
 			AvaloniaXamlLoader.Load(this);
 		}
 
+		// HACK: I really need to write some comments but I'm too lazy so have an emoji 😛
+
 		public void OnKeyDown(object sender, KeyEventArgs e)
 		{
-			this.FindControl<TextBox>("txt").Text = "FRED IS A BAD DOG";
-
+			
 		}
+
+		public void OnKeyUp(object sender, KeyEventArgs e)
+		{
+			char? thisChar = e.Key.ToString()[0];
+			if (lastChar is not null)
+			{
+				string prefix = $"{lastChar}{thisChar}";
+				if (prefix.Length >= 2)
+				{
+					var word = AutoComplete(prefix);
+					if (word is not null)
+					{
+						var txt = this.FindControl<TextBox>("txt");
+						string suffix = word.Substring(prefix.Length - 1);
+						txt.Text += suffix + " ";
+						txt.CaretIndex += suffix.Length + 1;
+						thisChar = null;
+					}
+				}
+			}
+			lastChar = thisChar;
+		}
+
+		private char? lastChar;
+
+		private string? AutoComplete(string start)
+		{
+			return Words.FirstOrDefault(q => q.ToLower().StartsWith(start.ToLower()));
+		}
+
+		private string[] Words { get; } = File.ReadAllLines("Assets/Words.txt");
 	}
 }
